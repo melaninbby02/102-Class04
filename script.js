19a292377c76ae8002b59ad3aa91889a187bd54f
Miriam Pendleton <123972418+melaninbby02@users.noreply.github.com>
2023-02-09 10:59:58 -0600

    These are just our edit of our webpage about our 5 favorite's animes we made. we just updated our paragraphs and images

---
+++ script.js
@@ -0,0 +1,34 @@
+let a = 1;
+
+function lesson = function (x) {
+    let b = 2;
+
+    console.log(x + b)
+
+
+
+
+
+
+}
+
+lesson(a);
+12();
+
+function whodat(){
+    
+    let name = prompt("What is your name");
+    //let name = console.Readline();
+    if (name != 'MIMI') {
+        alert("ERROR");
+    } else { 
+        alert("Hello, "+ name);
+    }
+
+    confirm
+
+    
+}
+
+//console.log(b);
+
