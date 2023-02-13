a2a0daa823cfb683eb967a44745de4d652526724
Miriam Pendleton <123972418+melaninbby02@users.noreply.github.com>
2023-02-13 11:44:51 -0600

    changes I made to my html is just editing my index.html for my web page.
    changes I made to my html is just editing my index.html for my web page.

--- script.js
+++ script.js
@@ -1,34 +1,4 @@
-let a = 1;
 
-function lesson = function (x) {
-    let b = 2;
-
-    console.log(x + b)
-
-
-
-
-
-
-}
-
-lesson(a);
-12();
-
-function whodat(){
-    
-    let name = prompt("What is your name");
-    //let name = console.Readline();
-    if (name != 'MIMI') {
-        alert("ERROR");
-    } else { 
-        alert("Hello, "+ name);
-    }
-
-    confirm
-
-    
-}
-
-//console.log(b);
-
+prompt("what is your name?")
+let name = "mimi"
+alert ("thats a nice name")
